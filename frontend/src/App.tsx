d43d7011d0c783a09eb21db4a2db323fb0b32c27
jwill1796 <jwills1796@gmail.com>
2025-03-02 07:52:07 -0600

    Complete overhaul of EngiCalc website, to include ability add typescript and work in react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Support from "./pages/Support";
+import FAQs from "./pages/FAQs";
+import DarkBackground from "./assets/Dark.png"; // Background image
+
+const App: React.FC = () => {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${DarkBackground})`,
+        backgroundSize: "contain",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/support" element={<Support />} />
+          <Route path="/faqs" element={<FAQs />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/Support.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Support: React.FC = () => {
+  return (
+    <div className = "home-container">
+    <div className="container">
+      <h1>EngiCalc - User Support</h1>
+
+      <div className="faq-section">
+        <h2>🐞 What if I find a bug?</h2>
+        <p>Please contact us at <strong>Engicalc-devteam-access@outlook.com</strong>.</p>
+
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default Support;
